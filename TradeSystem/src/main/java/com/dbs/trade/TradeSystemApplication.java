9a7d9370bf39dcece9ffac830007dc321d17e4c9
prabhas <prabhas@bahubali.com>
2021-08-20 09:09:17 +0000

    New ER diagram added

---
+++ TradeSystem/src/main/java/com/dbs/trade/TradeSystemApplication.java
@@ -0,0 +1,14 @@
+package com.dbs.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSystemApplication.class, args);
+		System.out.println("hello Started");
+	}
+
+}
